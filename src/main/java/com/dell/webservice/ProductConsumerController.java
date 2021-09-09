d98c028691ce1e91445c0e39f8552fcdad93190c
Anjali Kumari Agrawal <Anjali_Agrawal@Dell.com>
2021-09-09 18:38:54 +0530

    phase3-spring-consumer-webservice

---
+++ src/main/java/com/dell/webservice/ProductConsumerController.java
@@ -0,0 +1,46 @@
+package com.dell.webservice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.dell.webservice.entity.Product;
+
+@RestController
+@RequestMapping("/api/consumer")
+public class ProductConsumerController {
+	
+	@GetMapping("/products/{id}")
+	public Product fetchProduct(@PathVariable("id") long id) {
+		//consume api
+		//define a http url
+		String url = "http://localhost:9000/api/products/"+id; //consuming it in our app
+		
+		//use rest template
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getForEntity(url,  Product.class);
+		ResponseEntity<Product> product = restTemplate.getForEntity(url, Product.class);
+		return product.getBody();
+	}
+	@PutMapping("/products/{id}")
+	public String updateProduct(@PathVariable("id") long id, @RequestBody Product product) {
+		//consume api
+		//define a http url
+		String url = "http://localhost:9000/api/products/"+id; //consuming it in our app
+		
+		//use rest template
+		RestTemplate restTemplate = new RestTemplate();
+		
+		restTemplate.put(url, product);
+
+
+        return "product updated";
+	}
+	
+
+}
